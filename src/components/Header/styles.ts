3bb0aab908bec5f0dd7ef7e73d8be286c3a54387
Tayse <tayse_rosa@yahoo.com.br>
2021-12-02 11:34:19 -0300

    Concluindo Component Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -8,7 +8,7 @@ export const Content = styled.div`
   max-width: 1120px;
   margin: 0 auto;
 
-  padding: 2rem 1rem 10rem;
+  padding: 2rem 1rem 12rem;
 
   display: flex;
   align-items: center;
@@ -21,5 +21,11 @@ export const Content = styled.div`
       padding:0 2rem;
       border-radius: 0.25rem;
       height:3rem;
+
+      transition: filter 0.2s;
+
+      &:hover{
+        filter: brightness(0.9);
+      }
     }
 `;
